04395798b63c29f0240ce8dc2bc40a28ca066469
Marija Jelicic <marija.jelicic@netcast.rs>
2019-12-20 15:14:44 +0100

    webhook test

--- src/main/java/rs/raf/quotes/QuotesApplication.java
+++ src/main/java/rs/raf/quotes/QuotesApplication.java
@@ -2,10 +2,10 @@ package rs.raf.quotes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 public class QuotesApplication {
 
 	public static void main(String[] args) {
